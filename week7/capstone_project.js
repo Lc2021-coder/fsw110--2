922f3851f5e7ac7318d3fb0d7d2e6f7dc6a6c4d6
Lc2021-coder <81456231+Lc2021-coder@users.noreply.github.com>
2021-11-18 16:14:54 -0800

    Add files via upload

---
+++ week7/capstone_project.js
@@ -0,0 +1,34 @@
+const toDoForm = document.to_do_form;
+toDoForm.addEventListener("submit", e => {
+    e.preventDefault()
+    let toDo = toDoForm.to_do.value;
+    let toDoDescription = toDoForm.to_do_description.value;
+    let toDoItems = document.getElementById('to_do_items');
+    let newDiv = document.createElement('div');
+    let spanToDo = document.createElement('span');
+    let spanDelete = document.createElement('span');
+    let spanDescription = document.createElement('span');
+    let deleteBtn = document.createElement('button');
+    newDiv.style.width = '300px';
+    spanToDo.style.fontWeight = 'bold';
+    spanDescription.style.display = 'block';
+    deleteBtn.innerText = "Delete";
+    spanToDo.textContent = toDo;
+    spanDescription.textContent = toDoDescription;
+    with (deleteBtn) {
+        addEventListener('click', e => {
+            newDiv.remove();
+        });
+    }
+    spanDelete.appendChild(deleteBtn);
+    with (newDiv) {
+        appendChild(spanToDo);
+        appendChild(spanDelete);
+        appendChild(spanDescription);
+    }
+    toDoItems.appendChild(newDiv);
+    with (toDoForm) {
+        to_do.value = '';
+        to_do_description.value = '';
+    }
+});
